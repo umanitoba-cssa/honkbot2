5cf06e8de5484db7fd4749e8dc55700451f18448
travisfriesen <github@travisfriesen.ca>
2026-06-14 23:40:24 -0400

    fix: await interaciton.fetchReply()

--- bot/discord/modals/verification/VerifyStudentModal.ts
+++ bot/discord/modals/verification/VerifyStudentModal.ts
@@ -148,10 +148,12 @@ export namespace VerifyStudentModal {
             `- If you do not receive an email within 30 minutes, message the bot for assistance.`
         ].join("\n");
 
-        const replyMsg = (await interaction.editReply({
+        await interaction.editReply({
             content: response,
             components: [buttonRow]
-        })) as Message;
+        })
+
+        const replyMsg = await interaction.fetchReply();
 
         try {
             const nicknameButton = await replyMsg.awaitMessageComponent({
