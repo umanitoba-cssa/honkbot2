cad670395c933985e4113a708a63824d158079b4
Aidan McLeod <45048893+ACM02@users.noreply.github.com>
2026-08-18 21:59:08 -0500

    Hook up deploy commands to app startpoint
    - The app will now sync its commands with the server on boot, instead of having to run it manually

--- bot/index.ts
+++ bot/index.ts
@@ -1,10 +1,9 @@
-import { Client, ClientApplication, ClientPresence, Collection, GatewayIntentBits, Partials } from "discord.js";
+import { Client, GatewayIntentBits, Partials } from "discord.js";
 import fs from "fs";
 import path from "path";
 import { LoadAllModules } from "./data/Registry";
 
 // Check for required environment variables
-
 if (!process.env.DISCORD_CLIENT_ID) {
     throw new Error("DISCORD_CLIENT_ID is not set");
 }
@@ -40,16 +39,28 @@ if (!process.env.MYSQL_PASSWORD) {
 
 // Initialize discord.js client
 const client: Client = new Client({
-    intents: [GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMembers, GatewayIntentBits.Guilds, GatewayIntentBits.GuildModeration, GatewayIntentBits.DirectMessages, GatewayIntentBits.AutoModerationExecution],
-    partials: [Partials.Channel, Partials.Message, Partials.Reaction]
+    intents: [
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMessageReactions,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildModeration,
+        GatewayIntentBits.DirectMessages,
+        GatewayIntentBits.AutoModerationExecution,
+    ],
+    partials: [Partials.Channel, Partials.Message, Partials.Reaction],
 });
+
+// Load all modules into registry memory
 await LoadAllModules();
+
+// Deploy/Sync slash commands with Discord API
+await import("./deploy-commands"); // Or: import "./deploy-commands";
 
 // Load discord.js event handlers
 const eventsPath = path.join(import.meta.dir, "events");
 const eventFiles = fs.readdirSync(eventsPath).filter((file) => file.endsWith(".ts"));
-
-
 
 for (const file of eventFiles) {
     const filePath = path.join(eventsPath, file);
